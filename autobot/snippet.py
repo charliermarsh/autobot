ea23083d7ba34e192caa1870b750d2fc0838a246
Charlie Marsh <charlie.r.marsh@gmail.com>
2022-09-14 14:35:59 -0400

    Simplify snippet iteration

--- autobot/snippet.py
+++ autobot/snippet.py
@@ -5,14 +5,14 @@ import re
 from typing import Generator, NamedTuple, Type
 
 
-def decontextualize(source: str, node: ast.stmt) -> "Snippet":
+def decontextualize(source_code: str, node: ast.stmt) -> "Snippet":
     """Decontextualize a snippet from its originating source code.
 
     Takes the originating source code as input, along with the node to decontextualize,
     and extracts the code as a snippet, removing any indentation.
     """
     # Extract the source segment.
-    source_segment = ast.get_source_segment(source, node, padded=True)
+    source_segment = ast.get_source_segment(source_code, node, padded=True)
     assert source_segment, "Unable to find source segment."
 
     # Dedent the code:.
@@ -28,7 +28,7 @@ def decontextualize(source: str, node: ast.stmt) -> "Snippet":
     return Snippet(source_segment, padding, node.lineno)
 
 
-def recontextualize(snippet: "Snippet", source: str) -> list[str]:
+def recontextualize(snippet: "Snippet", source_code: str) -> list[str]:
     """Recontextualize a snippet within its originating source code.
 
     Takes the originating source code and snippet as input, and outputs the lines of the
@@ -38,7 +38,7 @@ def recontextualize(snippet: "Snippet", source: str) -> list[str]:
     lines: list[str] = []
 
     # Prepend any lines of the originating source code that precede the snippet.
-    source_lines = source.splitlines()
+    source_lines = source_code.splitlines()
     if snippet.lineno > 1:
         for i in range(snippet.lineno - 1):
             lines.append(source_lines[i])
@@ -58,32 +58,12 @@ class Snippet(NamedTuple):
     lineno: int
 
     @classmethod
-    def from_node(cls, source: str, node: ast.stmt) -> "Snippet":
-        return decontextualize(source, node)
-
-
-class ClassDefVisitor(ast.NodeVisitor):
-    """NodeVisitor to collect all ClassDef nodes in an AST."""
-
-    def __init__(self) -> None:
-        self.nodes: list[ast.ClassDef] = []
-
-    def visit_ClassDef(self, node: ast.ClassDef) -> None:
-        self.nodes.append(node)
-
-
-class FunctionDefVisitor(ast.NodeVisitor):
-    """NodeVisitor to collect all FunctionDef nodes in an AST."""
-
-    def __init__(self) -> None:
-        self.nodes: list[ast.FunctionDef] = []
-
-    def visit_FunctionDef(self, node: ast.FunctionDef) -> None:
-        self.nodes.append(node)
+    def from_node(cls, source_code: str, node: ast.stmt) -> "Snippet":
+        return decontextualize(source_code, node)
 
 
 def iter_snippets(
-    source: str,
+    source_code: str,
     node_type: Type[ast.stmt],
 ) -> Generator[Snippet, None, None]:
     """Generate all snippets from the provided source code.
@@ -91,15 +71,6 @@ def iter_snippets(
     Returns: a tuple of (text to fix, any indentation that was removed from the
         snippet, line number in the source file).
     """
-    visitor: ClassDefVisitor | FunctionDefVisitor
-    if node_type == ast.ClassDef:
-        visitor = ClassDefVisitor()
-    elif node_type == ast.FunctionDef:
-        visitor = FunctionDefVisitor()
-    else:
-        raise NotImplementedError(f"Unhandled AST node type: {node_type}")
-
-    visitor.visit(ast.parse(source))
-
-    for node in visitor.nodes:
-        yield Snippet.from_node(source, node)
+    for node in ast.walk(ast.parse(source_code)):
+        if isinstance(node, node_type):
+            yield Snippet.from_node(source_code, node)
