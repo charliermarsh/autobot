9102d102fc75fd3c32e5df489609400d05f8f03c
Charlie Marsh <charlie.r.marsh@gmail.com>
2022-09-22 15:39:53 -0400

    Add a unittest-to-pytest example

---
+++ autobot/schematics/unittest_to_pytest/after.py
@@ -0,0 +1,8 @@
+"""...as a pytest test suite instead."""
+
+
+def test_method() -> None:
+    expected = 3
+    actual = 1 + 2
+
+    assert expected == actual
---
+++ autobot/schematics/unittest_to_pytest/before.py
@@ -0,0 +1,8 @@
+"""...as a unittest.TestCase."""
+
+
+def test_method(self) -> None:
+    expected = 3
+    actual = 1 + 2
+
+    self.assertEqual(expected, actual)
